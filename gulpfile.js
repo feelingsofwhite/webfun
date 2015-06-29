58b49466dfe17154b97a5f986865bc4ba96f610b
James Keller <james@feelingsofwhite.com>
2015-06-28 23:33:36 -0600

    gulp just said hello world

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var console = require('console');
+
+
+gulp.task('default', function() {
+  // place code for your default task here
+  console.info('hello from gulp world');
+});
+
