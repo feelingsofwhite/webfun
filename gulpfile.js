6d936dd2230335384e6a27295b177ecae3276c42
James Keller <james@feelingsofwhite.com>
2015-06-29 21:41:50 -0600

    using gulp-connect to launch a webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var console = require('console');
 var del = require('del');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
 
 gulp.task('clean', function() {
     console.log('removing dest/**/*')
@@ -16,9 +18,32 @@ gulp.task('copyLibs', function(){
 
 gulp.task('copySrc', function(){
     gulp.src('src/**/*')
-        .pipe(gulp.dest('dest'));
+        .pipe(gulp.dest('dest'))
+        .pipe(connect.reload());
 });
 
+
+gulp.task('connect', function() {
+  var port = 2001; //a space odyssey
+  console.log('server started')
+  connect.server({
+    root: 'dest',
+    livereload: true,
+    port: port
+  });
+  console.log('server finished')
+});
+
+gulp.task('launch', function(){
+  console.log('launching browser')
+  var options = {
+    url: 'http://localhost:2001'
+    //app: 'google-chrome'
+  }
+  gulp
+    .src('src/index.html')  //src works, dest doesn't :(
+    .pipe(open('', options));
+}) 
 gulp.task('watch', function() {
     console.log('watching...');
     var watcher = gulp.watch('src/**/*', ['copySrc']);
@@ -28,4 +53,4 @@ gulp.task('watch', function() {
 
 });
 
-gulp.task('default', ['clean', 'build', 'watch']);
+gulp.task('default', ['clean', 'build', 'connect', 'launch', 'watch']);
